3c56ba730606c86a49dd886e9c8096226001d433
zhanghuihb <zhanghuihb@126.com>
2020-11-10 19:43:32 +0800

    芒果首页接口

---
+++ user/src/main/java/com/mongoit/user/controller/mongo/IndexController.java
@@ -0,0 +1,16 @@
+package com.mongoit.user.controller.mongo;
+
+import com.mongoit.common.base.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tainy
+ * @date 2020/11/10 0010 19:41
+ */
+@RestController
+@RequestMapping("/index")
+public class IndexController extends BaseController {
+
+
+}
